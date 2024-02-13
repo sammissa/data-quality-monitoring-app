b7268573fdd5a72da24b7d7e678871fc21e78dc9
Sammy Issa <sam_issa@hotmail.co.uk>
2024-02-13 08:48:10 +0000

    Update .eslintrc.js
    - fix lint workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     'es2021': true
   },
   'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'eslint:recommended'
   ],
   'overrides': [
     {
